743c91bd7b52995c56b6fa5efd90fd01c2d49845
Daniel Porteous <danielporteous1@gmail.com>
2018-01-12 23:44:44 +1100

    Polish and avoiding data leakage

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ MAIN_START = '2016-01-05'
 d = {
     'Alice':
     [
-        Duration(MAIN_START, '2017-01-05', '', True),
+        Duration(MAIN_START, '2017-01-01', '', True),
         Duration('2017-01-02', '2017-04-01', 'In America', False),
         Duration('2017-04-02', '', '', False),
     ],
